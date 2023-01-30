15685adbcc5dbbfba1092c9079e6886ff3f30618
Himanshu Singh Rawat <himanshu.s.rawat2001@gmail.com>
2023-01-30 22:57:59 +0530

    final_integrated_col_uart_line

--- final code/Line_Follower.v
+++ final code/Line_Follower.v
@@ -35,7 +35,9 @@ module Line_Follower (
 
 reg tled = 0;
 assign ledt = tled;
-////// --- Colour sensor params ////////////////////////
+
+//// UART_COLOUR - INTEGRATED :) DECL///////
+
 reg temp_col_freq;
 reg [100:0] act_freq_cnt = 0;
 reg [100:0] cnt_freq_str = 0;
@@ -50,8 +52,222 @@ reg [100:0] COUNT_RED = 0;
 reg [100:0] COUNT_GREEN = 0;
 reg [100:0] COUNT_BLUE = 0;
 
-///////////////////////////////////////////////////////////////
-always @ (posedge clk) begin
+reg [10:0] uart_count1 = 0; 
+reg [10:0] uart_count2 = 0; 
+reg [10:0] uart_count3 = 0; 
+reg Tx = 1 ;
+assign tx = Tx ;
+
+
+
+localparam 
+				idle            = 5'b00001 ,
+				start           = 5'b00010 ,
+				stop            = 5'b00011 ,
+				terminate       = 5'b00100 ,
+				G_letter        = 5'b00101 ,
+				B_letter        = 5'b00110 ,
+				I_letter        = 5'b00111 ,
+				Number_letter   = 5'b01000 ,
+				Dash_letter_1   = 5'b01001 ,
+            Variable_letter = 5'b01010 ,				
+				Dash_letter_2   = 5'b01011 ,
+				Hash_letter     = 5'b01100 ,
+				Null_letter     = 5'b01101 ;
+				
+			
+reg [8:0]counter = 9'b00 ; 				
+reg [8:0]cs_uart = idle ;	
+reg [8:0]data = 5'b00101;
+reg [8:0]bit_counter = 4'b00 ;
+	
+reg [7:0]g_letter       = 8'b01000111 ;
+reg [7:0]b_letter       = 8'b01000010 ;                 
+reg [7:0]i_letter       = 8'b01001001 ;
+reg [7:0]dash_letter_1  = 8'b00101101 ;
+reg [7:0]m_letter       = 8'b01001101 ;
+reg [7:0]d_letter       = 8'b01000100 ;
+reg [7:0]w_letter       = 8'b01010111 ; 
+reg [7:0]letter_1       = 8'b00110001 ;
+reg [7:0]letter_2       = 8'b00110010 ;
+reg [7:0]letter_3       = 8'b00110011 ;
+reg [7:0]dash_letter_2  = 8'b00101101 ;
+reg [7:0]hash_letter    = 8'b00100011 ;
+reg [7:0]null_letter    = 8'b00000000 ;
+
+reg [7:0] variable_letter  [2:0];
+reg [7:0] variable_number  [2:0];
+
+reg [3:0]variable_cnt = 0;
+reg [3:0]variable_num_cnt = 0;
+reg [5:0] cnt_uart = 0 ;
+reg uart_flag = 0 ;
+
+
+//// uart  colour code///////
+
+always @ (posedge clk )
+begin
+variable_letter[0] = 8'b01001101;
+variable_letter[1] = 8'b01000100;
+variable_letter[2] = 8'b01010111;
+variable_number[0] = 8'b00110001;
+variable_number[1] = 8'b00110010;
+variable_number[2] = 8'b00110011;
+if (uart_flag == 1 )
+begin 
+
+if (counter == 0 || counter == 9'b110110010 )
+begin 
+
+
+case (cs_uart)
+
+			idle :
+					begin 
+					Tx = 1 ;
+					cs_uart = start ;
+					end 
+					
+			start : 
+					 begin 
+					 Tx = 0 ;
+//					 tled = 1;
+					 cs_uart = data ;
+					 data = data + 1 ;
+					 end 
+					 
+			stop :
+					 begin 
+					 Tx = 1 ;
+					 cs_uart = idle ;
+					 end 
+					 
+			terminate :
+						begin 
+						Tx = 1 ;
+						counter = 9'b00 ; 	
+						cs_uart = idle ;	
+						data = 5'b00101;
+						bit_counter = 4'b00 ;
+						uart_flag = 0 ;
+						end 
+						
+			G_letter :
+						begin 
+						Tx = g_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+						
+						
+			B_letter :
+						begin 
+						Tx = b_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+
+			I_letter :
+						begin 
+						Tx = i_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+						
+						
+		  Number_letter :
+						begin 
+						Tx = variable_number[variable_num_cnt][bit_counter] ;	
+						
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cnt_uart = cnt_uart + 1 ;
+						cs_uart = stop  ;
+						end 
+						end 
+
+			Dash_letter_1 :
+						begin 
+						Tx = dash_letter_1[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+
+		  Variable_letter :
+						begin
+						
+						Tx = variable_letter[variable_cnt][bit_counter] ;	
+						bit_counter =bit_counter  + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 					
+						
+			Dash_letter_2 :
+						begin 
+						Tx = dash_letter_2[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+			
+			Hash_letter :
+						begin 
+						Tx = hash_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+						
+			 Null_letter :
+						begin 
+						Tx = null_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = terminate  ;
+						end 
+						end 
+			
+
+endcase 
+end 
+
+if (counter == 9'b110110010)
+begin
+	counter = 9'b0 ;
+end 
+counter = counter + 1 ;
+end
+
 		temp_col_freq <= colour_freq;
 		if (colour_freq)
 			cnt_freq_str <= cnt_freq_str + 1;
@@ -62,41 +278,61 @@ always @ (posedge clk) begin
 		if (act_freq_cnt>1200 & act_freq_cnt<1500)
 		begin
 			COUNT_RED = COUNT_RED + 1;
-			if (COUNT_RED == 40000000) begin
+			if (COUNT_RED == 30000000) begin
 			COUNT_RED = 0;
 			COUNT_GREEN = 0;
 			COUNT_BLUE = 0;
 			rled = 1;
 			gled = 0;
 			bled = 0;
+			variable_cnt = 0;
+			variable_num_cnt = 0;
+			uart_count1=0;
+			uart_count2=0;
+			uart_count3 = uart_count3 +1;
+
+			if (uart_count3<2)begin uart_flag = 1;end
 			end
 		end
 		else if (act_freq_cnt>2300 & act_freq_cnt< 2700)
 		begin
 		COUNT_GREEN = COUNT_GREEN + 1;
-		if (COUNT_GREEN == 40000000) begin
+		if (COUNT_GREEN == 30000000) begin
 			COUNT_RED = 0;
 			COUNT_GREEN = 0;
 			COUNT_BLUE = 0;
 			rled = 0;
 			gled = 1;
 			bled = 0;
+			uart_count2 = uart_count2 +1;
+		variable_cnt = 1;
+		variable_num_cnt = 1;
+		uart_count1=0;
+		uart_count3=0;
+		if (uart_count2 <2)begin uart_flag = 1;end
 			end
 		end	
 		else if (act_freq_cnt>3900 & act_freq_cnt<4500)
 		begin
 			COUNT_BLUE = COUNT_BLUE + 1;
-		if (COUNT_BLUE == 40000000) begin
+		if (COUNT_BLUE == 30000000) begin
 			COUNT_RED = 0;
 			COUNT_GREEN = 0;
 			COUNT_BLUE = 0;
 			rled = 0;
 			gled = 0;
 			bled = 1;
+			uart_count1 = uart_count1 +1;
+		variable_cnt = 2;
+		variable_num_cnt = 2;
+		uart_count2=0;
+		uart_count3=0;
+		if (uart_count1<2)begin uart_flag = 1;end
 		end
 		end
-	end
-	
+
+
+end	
 ////// --- ADC PARAMETERS INITALIZATION //////////////////////// 
 	
 	
@@ -419,213 +655,7 @@ reg flag_wait_lobby = 0 ;
 reg [10:0] cnt_wait_lobby = 0 ;
 
 
-////////// UART //////////////////////
-
-reg Tx = 1 ;
-assign tx = Tx ;
-
-
-
-localparam 
-				idle            = 5'b00001 ,
-				start           = 5'b00010 ,
-				stop            = 5'b00011 ,
-				terminate       = 5'b00100 ,
-				G_letter        = 5'b00101 ,
-				B_letter        = 5'b00110 ,
-				I_letter        = 5'b00111 ,
-				Number_letter   = 5'b01000 ,
-				Dash_letter_1   = 5'b01001 ,
-            Variable_letter = 5'b01010 ,				
-				Dash_letter_2   = 5'b01011 ,
-				Hash_letter     = 5'b01100 ,
-				Null_letter     = 5'b01101 ;
-				
-			
-reg [8:0]counter = 9'b00 ; 				
-reg [2:0]cs_uart = idle ;	
-reg [2:0]data = 5'b00101;
-reg [3:0]bit_counter = 4'b00 ;
-	
-reg [7:0]g_letter       = 8'b01000111 ;
-reg [7:0]b_letter       = 8'b01000010 ;                 
-reg [7:0]i_letter       = 8'b01101001 ;
-reg [7:0]dash_letter_1  = 8'b00100011 ;
-reg [7:0]m_letter       = 8'b01001101 ;
-reg [7:0]d_letter       = 8'b01000100 ;
-reg [7:0]w_letter       = 8'b01010111 ; 
-reg [7:0]letter_1       = 8'b00110001 ;
-reg [7:0]letter_2       = 8'b00110010 ;
-reg [7:0]letter_3       = 8'b00110011 ;
-reg [7:0]dash_letter_2  = 8'b00101101 ;
-reg [7:0]hash_letter    = 8'b00100011 ;
-reg [7:0]null_letter    = 8'b00000000 ;
-
-reg [5:0] cnt_uart = 0 ;
-reg uart_flag = 0 ;
-
-always @ (posedge clk )
-begin
-if (uart_flag == 1 )
-begin 
-
-if (counter == 0 || counter == 9'b110110010 )
-begin 
-
-
-case (cs_uart)
-
-			idle :
-					begin 
-					Tx = 1 ;
-					cs_uart = start ;
-					end 
-					
-			start : 
-					 begin 
-					 Tx = 0 ;
-//					 tled = 1;
-					 cs_uart = data ;
-					 data = data + 1 ;
-					 end 
-					 
-			stop :
-					 begin 
-					 Tx = 1 ;
-					 cs_uart = idle ;
-					 end 
-					 
-			terminate :
-						begin 
-						Tx = 1 ;
-						counter = 9'b00 ; 	
-						cs_uart = idle ;	
-						data = 5'b00101;
-						bit_counter = 4'b00 ;
-						uart_flag = 0 ;
-						flag_decision = 1 ;
-						flag_wait_lobby = 0;
-						end 
-						
-			G_letter :
-						begin 
-						Tx = g_letter[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 
-						
-						
-			B_letter :
-						begin 
-						Tx = b_letter[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 
-
-			I_letter :
-						begin 
-						Tx = i_letter[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 
-						
-						
-		  Number_letter :
-						begin 
-						if (cnt_uart == 0 ) begin Tx = letter_1[bit_counter] ; end
-						if (cnt_uart == 1 ) begin Tx = letter_2[bit_counter] ; end
-                  if (cnt_uart == 2 ) begin Tx = letter_3[bit_counter] ; end
-						
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cnt_uart = cnt_uart + 1 ;
-						cs_uart = stop  ;
-						end 
-						end 
-
-			Dash_letter_1 :
-						begin 
-						Tx = Dash_letter_1[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 
-
-		  Variable_letter :
-						begin
-						
-						if (cnt_uart == 1 ) begin Tx = m_letter[bit_counter] ; end
-						if (cnt_uart == 2 ) begin Tx = d_letter[bit_counter] ; end
-                  if (cnt_uart == 3 ) begin Tx = w_letter[bit_counter] ; end	
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 					
-						
-			Dash_letter_2 :
-						begin 
-						Tx = Dash_letter_2[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 
-			
-			hash_letter :
-						begin 
-						Tx = hash_letter[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = stop  ;
-						end 
-						end 
-						
-			 null_letter :
-						begin 
-						Tx = null_letter[bit_counter] ;
-						bit_counter = bit_counter + 1 ;
-						if (bit_counter == 4'b1000)
-						begin 
-						bit_counter = 0 ;
-						cs_uart = terminate  ;
-						end 
-						end 
-			
-
-endcase 
-end 
-
-if (counter == 9'b110110010)
-begin
-	counter = 9'b0 ;
-end 
-counter = counter + 1 ;
-end 
-
+always @ (posedge clk) begin
 if ( flag_decision == 1 )
 begin
 
@@ -886,42 +916,6 @@ begin
 end 	
 
 	
-if (flag_wait_lobby ==  1 )
-begin
-	
-
-	if ( cnt_wait_lobby == 3 )
-	begin
-	uart_flag = 1 ;
-		
-	
-	end 
-
-   else if ( cnt_wait_lobby == 6 )
-	begin
-	tled=1;
-		uart_flag = 1 ;
-	
-	end 
-
-  else if ( cnt_wait_lobby == 8 )
-	begin
-	
-	uart_flag = 1 ;
-		
-	
-	end 
-  
-  else 
-  begin
-  
-  		flag_decision  =  1  ;
-  
-  end 
-	
-  cnt_wait_lobby = cnt_wait_lobby + 1 ;	
-
-end 	
 
 
 if (flag_pwm == 1 )
---
+++ working-integration-col-and-uart/col_sen.v
@@ -0,0 +1,308 @@
+
+module col_sen(
+	input clk,
+	input colour_freq,
+	output red_led,
+	output blue_led,
+	output tx,
+	output green_led
+	
+);
+
+
+reg temp_col_freq;
+reg [100:0] act_freq_cnt = 0;
+reg [100:0] cnt_freq_str = 0;
+reg [6:0] pwm_cnt = 0;
+
+reg rled=0;
+reg gled=0;
+reg bled=0;
+assign red_led = rled;
+assign green_led =gled;
+assign blue_led = bled;
+reg [100:0] COUNT_RED = 0;
+reg [100:0] COUNT_GREEN = 0;
+reg [100:0] COUNT_BLUE = 0;
+
+reg [10:0] uart_count1 = 0; 
+reg [10:0] uart_count2 = 0; 
+reg [10:0] uart_count3 = 0; 
+reg Tx = 1 ;
+assign tx = Tx ;
+
+
+
+localparam 
+				idle            = 5'b00001 ,
+				start           = 5'b00010 ,
+				stop            = 5'b00011 ,
+				terminate       = 5'b00100 ,
+				G_letter        = 5'b00101 ,
+				B_letter        = 5'b00110 ,
+				I_letter        = 5'b00111 ,
+				Number_letter   = 5'b01000 ,
+				Dash_letter_1   = 5'b01001 ,
+            Variable_letter = 5'b01010 ,				
+				Dash_letter_2   = 5'b01011 ,
+				Hash_letter     = 5'b01100 ,
+				Null_letter     = 5'b01101 ;
+				
+			
+reg [8:0]counter = 9'b00 ; 				
+reg [8:0]cs_uart = idle ;	
+reg [8:0]data = 5'b00101;
+reg [8:0]bit_counter = 4'b00 ;
+	
+reg [7:0]g_letter       = 8'b01000111 ;
+reg [7:0]b_letter       = 8'b01000010 ;                 
+reg [7:0]i_letter       = 8'b01001001 ;
+reg [7:0]dash_letter_1  = 8'b00101101 ;
+reg [7:0]m_letter       = 8'b01001101 ;
+reg [7:0]d_letter       = 8'b01000100 ;
+reg [7:0]w_letter       = 8'b01010111 ; 
+reg [7:0]letter_1       = 8'b00110001 ;
+reg [7:0]letter_2       = 8'b00110010 ;
+reg [7:0]letter_3       = 8'b00110011 ;
+reg [7:0]dash_letter_2  = 8'b00101101 ;
+reg [7:0]hash_letter    = 8'b00100011 ;
+reg [7:0]null_letter    = 8'b00000000 ;
+
+reg [7:0] variable_letter  [2:0];
+reg [7:0] variable_number  [2:0];
+
+reg [3:0]variable_cnt = 0;
+reg [3:0]variable_num_cnt = 0;
+reg [5:0] cnt_uart = 0 ;
+reg uart_flag = 0 ;
+
+always @ (posedge clk )
+begin
+variable_letter[0] = 8'b01001101;
+variable_letter[1] = 8'b01000100;
+variable_letter[2] = 8'b01010111;
+variable_number[0] = 8'b00110001;
+variable_number[1] = 8'b00110010;
+variable_number[2] = 8'b00110011;
+if (uart_flag == 1 )
+begin 
+
+if (counter == 0 || counter == 9'b110110010 )
+begin 
+
+
+case (cs_uart)
+
+			idle :
+					begin 
+					Tx = 1 ;
+					cs_uart = start ;
+					end 
+					
+			start : 
+					 begin 
+					 Tx = 0 ;
+//					 tled = 1;
+					 cs_uart = data ;
+					 data = data + 1 ;
+					 end 
+					 
+			stop :
+					 begin 
+					 Tx = 1 ;
+					 cs_uart = idle ;
+					 end 
+					 
+			terminate :
+						begin 
+						Tx = 1 ;
+						counter = 9'b00 ; 	
+						cs_uart = idle ;	
+						data = 5'b00101;
+						bit_counter = 4'b00 ;
+						uart_flag = 0 ;
+						end 
+						
+			G_letter :
+						begin 
+						Tx = g_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+						
+						
+			B_letter :
+						begin 
+						Tx = b_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+
+			I_letter :
+						begin 
+						Tx = i_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+						
+						
+		  Number_letter :
+						begin 
+						Tx = variable_number[variable_num_cnt][bit_counter] ;	
+						
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cnt_uart = cnt_uart + 1 ;
+						cs_uart = stop  ;
+						end 
+						end 
+
+			Dash_letter_1 :
+						begin 
+						Tx = dash_letter_1[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+
+		  Variable_letter :
+						begin
+						
+						Tx = variable_letter[variable_cnt][bit_counter] ;	
+						bit_counter =bit_counter  + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 					
+						
+			Dash_letter_2 :
+						begin 
+						Tx = dash_letter_2[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+			
+			Hash_letter :
+						begin 
+						Tx = hash_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = stop  ;
+						end 
+						end 
+						
+			 Null_letter :
+						begin 
+						Tx = null_letter[bit_counter] ;
+						bit_counter = bit_counter + 1 ;
+						if (bit_counter == 4'b1000)
+						begin 
+						bit_counter = 0 ;
+						cs_uart = terminate  ;
+						end 
+						end 
+			
+
+endcase 
+end 
+
+if (counter == 9'b110110010)
+begin
+	counter = 9'b0 ;
+end 
+counter = counter + 1 ;
+end
+
+		temp_col_freq <= colour_freq;
+		if (colour_freq)
+			cnt_freq_str <= cnt_freq_str + 1;
+		else if (temp_col_freq) begin
+			act_freq_cnt <= cnt_freq_str;
+			cnt_freq_str <= 0;
+		end
+		if (act_freq_cnt>1200 & act_freq_cnt<1500)
+		begin
+			COUNT_RED = COUNT_RED + 1;
+			if (COUNT_RED == 30000000) begin
+			COUNT_RED = 0;
+			COUNT_GREEN = 0;
+			COUNT_BLUE = 0;
+			rled = 1;
+			gled = 0;
+			bled = 0;
+			variable_cnt = 0;
+			variable_num_cnt = 0;
+			uart_count1=0;
+			uart_count2=0;
+			uart_count3 = uart_count3 +1;
+
+			if (uart_count3<2)begin uart_flag = 1;end
+			end
+		end
+		else if (act_freq_cnt>2300 & act_freq_cnt< 2700)
+		begin
+		COUNT_GREEN = COUNT_GREEN + 1;
+		if (COUNT_GREEN == 30000000) begin
+			COUNT_RED = 0;
+			COUNT_GREEN = 0;
+			COUNT_BLUE = 0;
+			rled = 0;
+			gled = 1;
+			bled = 0;
+			uart_count2 = uart_count2 +1;
+		variable_cnt = 1;
+		variable_num_cnt = 1;
+		uart_count1=0;
+		uart_count3=0;
+		if (uart_count2 <2)begin uart_flag = 1;end
+			end
+		end	
+		else if (act_freq_cnt>3900 & act_freq_cnt<4500)
+		begin
+			COUNT_BLUE = COUNT_BLUE + 1;
+		if (COUNT_BLUE == 30000000) begin
+			COUNT_RED = 0;
+			COUNT_GREEN = 0;
+			COUNT_BLUE = 0;
+			rled = 0;
+			gled = 0;
+			bled = 1;
+			uart_count1 = uart_count1 +1;
+		variable_cnt = 2;
+		variable_num_cnt = 2;
+		uart_count2=0;
+		uart_count3=0;
+		if (uart_count1<2)begin uart_flag = 1;end
+		end
+		end
+
+
+end
+	
+
+endmodule
